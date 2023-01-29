099821c41023a79c5641927560d1ec4f85c9ce5d
developer <andrewkrupenko338@gmail.com>
2023-01-29 18:57:49 +0200

    added jwt login functionality

--- models/userModel.js
+++ models/userModel.js
@@ -18,8 +18,6 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
-  const exists = await this.findOne({ email });
-
   if (!email || !password) {
     throw Error("All fields must be filled!");
   }
@@ -32,6 +30,8 @@ userSchema.statics.signup = async function (email, password) {
     throw Error("Password not strong enough");
   }
 
+  const exists = await this.findOne({ email });
+
   if (exists) {
     throw Error("Email already in use");
   }
@@ -43,6 +43,26 @@ userSchema.statics.signup = async function (email, password) {
     email,
     password: hash,
   });
+
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled!");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
   return user;
 };
