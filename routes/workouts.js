4ef06f935ad7eb1b7730933a6962ed30e68afd31
developer <andrewkrupenko338@gmail.com>
2023-02-05 22:18:31 +0200

    protecting api routes

--- routes/workouts.js
+++ routes/workouts.js
@@ -6,8 +6,12 @@ const {
   updateWorkoutItem,
   deleteWorkoutItem,
 } = require("../controllers/workoutController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth);
 
 router.get("/", getWorkouts);
 router.get("/:id", getWorkoutItem);
